a37a7c5162fe7529e46b242b8ae2998a10461a62
Infanttt <113401540+Adesaputraaa@users.noreply.github.com>
2023-05-13 15:35:56 +0700

    new
    lalalala

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 2000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+    document.getElementById("nav").style.background = "#03071e";
+  } else {
+    document.getElementById("nav").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
